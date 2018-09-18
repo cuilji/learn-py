0c199b51d29d9c493a81d013f2d9177b3d01cc79
cuilianji <cuilji@163.com>
2018-09-18 09:54:17 +0800

    add a verification data.

--- gdp.py
+++ gdp.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 #2000年gdp10万亿元
+#unit 亿元
 gdp=340903
+#Year2017gdp=827122
 for year in range(2010, 2046):
-    print('year' + str(year) +':'+ str(gdp*1.08))
+    print('Year' + str(year) +':'+ str(gdp*1.08))
     gdp=gdp*1.1
 
     
