b0acb0801d344c999eca1b4f1807e82e8e8b4087
cuilianji <cuilji@163.com>
2018-09-18 09:40:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- gdp.py
+++ gdp.py
@@ -1,8 +1,8 @@
-# -*- coding: cp936 -*-
+# -*- coding: utf-8 -*-
 #2000年gdp10万亿元
 gdp=340903
 for year in range(2010, 2046):
-    print 'year' + str(year) +':'+ str(gdp*1.08)
+    print('year' + str(year) +':'+ str(gdp*1.08))
     gdp=gdp*1.1
 
     
